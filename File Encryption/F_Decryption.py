e42dcc6758e89a0de95cd6988be549468874bd79
Garvit Agarwal <garvitagl@gmail.com>
2020-04-17 21:45:25 +0530

    Testing. File size issues.

--- File Encryption/F_Decryption.py
+++ File Encryption/F_Decryption.py
@@ -1,8 +1,7 @@
 from Crypto.Cipher import AES
 from init import key,iv
 from Padding import unpad
-sz = 4893*4
-from F_Encryption import data # There is no sense in adding this, however whenever this statement is removed, we face an error 'Padding is incorrent'. Just import anything from F_Encryption We'll Look into it
+import F_Encryption # There is no sense in adding this, however whenever this statement is removed, we face an error 'Padding is incorrent'. Just import anything from F_Encryption We'll Look into it
 with open('encfile.txt', 'rb') as fin:
     # iv = fin.read(16)
 
--- File Encryption/F_Encryption.py
+++ File Encryption/F_Encryption.py
@@ -1,9 +1,8 @@
 from Crypto.Cipher import AES
 from init import key,iv
 import os
-import struct
-from Padding import pad,unpad
-sz = 2048
+from Padding import pad
+abc = 0.1
 fsz = os.path.getsize('/Users/goofy/Desktop/test.txt')
 with open('big_test.txt') as finput:
     while True:
@@ -12,23 +11,12 @@ with open('big_test.txt') as finput:
         n = len(data)
         if n == 0:
             break
-        # elif n % 16 != 0:
-            # data += ' ' * (16 - n % 16)  # <- padded with spaces
+        elif n % 16 != 0:
+            data += ' ' * (16 - n % 16)  # <- padded with spaces
         aes = AES.new(key, AES.MODE_CBC, iv)
         with open('encfile.txt', 'wb') as fout:
             # fout.write(iv)
+            global encd
             encd = aes.encrypt(pad(data, 16))
-            #cd)
-            #
-
             fout.write(encd)
-            # fout.write(struct.pack('<Q', fsz))
 
-        # with open('/Users/goofy/Desktop/encfile.txt', 'wb') as fout:
-        #      fout.write(struct.pack('<Q', fsz))
-             #fout.write(iv)
-
-# if data == decd:
-#     print("Working with success")
-# else:
-#     print("Try again")
