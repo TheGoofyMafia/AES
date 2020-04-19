b6e81014b9c72164c51241103ce14f7058e20a9a
Garvit Agarwal <garvitagl@gmail.com>
2020-04-19 21:20:27 +0530

    More testing needed. Working on 300mb+ files.

--- File Encryption/test_generator.py
+++ File Encryption/test_generator.py
@@ -1,7 +1,7 @@
 import random
 import string
 with open('big_test.txt', 'w') as testfile:
-    sz = int(input('Size of file in bytes: '))
+    sz = int(input('Size of test file in bytes: '))
     random = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(sz)])
     testfile.write(random)
 
